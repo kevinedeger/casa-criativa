7e5ca7b4b01b48ba8b20fe4e000f2270dc472048
Kevin Eger <kevinegersz@gmail.com>
2020-03-28 19:23:30 -0300

    finaliza o projeto

--- server.js
+++ server.js
@@ -3,54 +3,18 @@
 const express = require("express")
 const server = express()
 
+const db = require("./db")
+
 // Compilado de Ideias
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Curso de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque quia earum",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque quia earum",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque quia earum",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em Família",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque quia earum",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title: "Pintura",
-        category: "Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque quia earum",
-        url: "https://rocketseat.com.br"
-    }, 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title: "Recortes",
-        category: "Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque quia earum",
-        url: "https://rocketseat.com.br"
-    },   
-]
+// const ideas = [
+// 
+// ]
 
 // Configurar arquivos estáticos (CSS, scripts, imagens)
 server.use(express.static("public"))
+
+// Habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 // Configuração do nunjucks
 const nunjucks = require("nunjucks")
@@ -63,25 +27,72 @@ nunjucks.configure("views", {
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res){
 
-    const reversedIdeas =  [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+    
+        const reversedIdeas =  [...rows].reverse()
 
-    let lastIdeas = []
-    for (idea of reversedIdeas) {
-        if(lastIdeas.length < 2) {
-            lastIdeas.push(idea)
+        let lastIdeas = []
+        for (idea of reversedIdeas) {
+            if(lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+
         }
 
-    }
+        return res.render("index.html", { ideas:lastIdeas })
+        })
 
-    return res.render("index.html", { ideas:lastIdeas })
+
 })
 
 server.get("/ideias", function(req, res){
 
-    const reversedIdeas =  [...ideas].reverse()
 
-    return res.render( "ideias.html", {ideas: reversedIdeas})
 
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        const reversedIdeas =  [...rows].reverse()
+
+        return res.render( "ideias.html", {ideas: reversedIdeas})
+
+    })
+})
+
+server.post("/", function(req, res){
+    // Inserir dados na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+        `
+    
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]    
+    
+    db.run(query, values, function(err){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+            return res.redirect ("/ideias")
+    })
 })
 
 // Liguei meu servidor na porta 3000
